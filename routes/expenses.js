705256f456d993d271b2a9c91548c23104ba2d24
Minna Nguyen <minna.nguyen@tuni.fi>
2022-12-28 20:26:10 +0200

    add new filter endpoints (id, month, category)

--- routes/expenses.js
+++ routes/expenses.js
@@ -1,8 +1,16 @@
 const express = require("express");
 
-const { getExpenses } = require("../controllers/expenses");
+const {
+  getExpenses,
+  getExpenseByType,
+  getExpenseById,
+  getByMonth,
+} = require("../controllers/expenses");
 const router = express.Router();
 
 router.get("/", getExpenses);
+router.get("/:id", getExpenseById);
+router.get("/category/:shop", getExpenseByType);
+router.get("/month/:month", getByMonth);
 
 module.exports = router;
