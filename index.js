c7f88381f11f5c8aea79343529f22131293dcf35
Minna Nguyen <minna.nguyen@tuni.fi>
2023-01-12 14:23:38 +0200

    add: Render health endpoint

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.use(express.json());
 
 // ths is the basic mount path
 app.use("/api/expenses", routes);
+
+app.get("/api/expenses/check/health", (req, res) => {
+  res.send("200 OK");
+});
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Backend listening to port ${PORT}`);
