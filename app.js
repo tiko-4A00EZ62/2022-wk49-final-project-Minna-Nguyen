f591b51ce1d28c4d000169a0e3fd4d38cc70b6a3
Minna Nguyen <minna.nguyen@tuni.fi>
2023-01-12 20:07:54 +0200

    create app.route

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const routes = require("./routes/expenses");
+
+const app = express();
+app.use(express.json());
+
+// ths is the basic mount path
+app.use("/api/expenses", routes);
+
+app.get("/api/expenses/check/health", (req, res) => {
+  res.send("200 OK");
+});
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-const express = require("express");
-const routes = require("./routes/expenses");
+const app = require("./app");
 
-const app = express();
-app.use(express.json());
-
-// ths is the basic mount path
-app.use("/api/expenses", routes);
-
-app.get("/api/expenses/check/health", (req, res) => {
-  res.send("200 OK");
-});
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Backend listening to port ${PORT}`);
