4e3f47fc079c6cfd8760bf4ace623f0982c63e19
Minna Nguyen <minna.nguyen@tuni.fi>
2023-01-12 20:22:46 +0200

    health point edit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 // ths is the basic mount path
 app.use("/api/expenses", routes);
 
-app.get("/api/expenses/check/health", (req, res) => {
+app.get("/health", (req, res) => {
   res.send("200 OK");
 });
 
