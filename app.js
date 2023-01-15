cc254b151c61129e87aca72b0c6ae1da71fff384
Minna Nguyen <minna.nguyen@tuni.fi>
2023-01-15 18:49:51 +0200

    app.js change cors order

--- app.js
+++ app.js
@@ -3,22 +3,21 @@ const cors = require("cors");
 const routes = require("./routes/expenses");
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "https://editor.swagger.io/",
-      "http://localhost:5001/",
-      "https://trackexpense.onrender.com/",
-    ],
-  })
-);
-
 app.use(express.json());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
+app.use(
+  cors({
+    origin: [
+      "https://editor.swagger.io",
+      "http://localhost:5001",
+      "https://trackexpense.onrender.com",
+    ],
+  })
+);
 // ths is the basic mount path
 app.use("/api/expenses", routes);
 app.get("/health", (req, res) => {
